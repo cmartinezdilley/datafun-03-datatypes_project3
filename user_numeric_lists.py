cad8aa7058905922d8780d6e55f59e92633363aa
Christine Martinez <you@example.com>
2023-02-07 21:08:34 -0600

    Project 3

--- user_numeric_lists.py
+++ user_numeric_lists.py
@@ -118,3 +118,93 @@ y_new=slope*x_max+intercept
 print(f"With linear regression if a cat is 25 years old, it will cost {y_new:.3f}")
 
 #list 3 project 3
+print("List number 3, Python Built in Functions")
+#Calculate min, max,len, sum, average, set, sorted, sorted, using reverse true
+
+min=min(listA)
+print(f"min is:{min}")
+
+max=max(listA)
+print(f"max is:{max}")
+
+len=len(listA)
+print(f"len is:{len}")
+
+sum=(sum(listA))
+print(f"sum is:{sum}")
+
+avg=sum/len
+print(f"avg is: {avg: .3f}")
+
+sorted=sorted(listA)
+print(f"sorted: {sorted}")
+
+print(f"List 4, project 3,Methods")
+#append,extend, insert, remove,count,sort, sort with reverse, copy, pop first, pop last
+
+lst= [10,7,3,1]
+print(f"New short list is: {lst}")
+print("Append a single value to the list:")
+lst.append(8)
+print(lst)
+print("Extend with a new list:")
+lst.extend([6,5,9])
+print(lst)
+
+lst.insert(11,14)
+print(f"Lst with insert: {lst}")
+
+remove_item=5
+lst.remove(remove_item)
+print(lst)
+
+print("count how many times there is a 2 in the list")
+count_of_two=lst.count(2)
+print(count_of_two)
+
+asc_list= lst.sort()
+print(asc_list)
+
+print("This is the list sorted in descending order)")
+descending_sorted= lst.sort(reverse=True)
+print(descending_sorted)
+
+print("This is a copy of the list")
+lst_1=lst.copy()
+print(lst_1)
+
+print("List with the first item removed")
+first_item=lst_1.pop(0)
+print(lst_1)
+
+print("List with the last item removed")
+last_item=lst_1.pop(-1)
+print(lst_1)
+
+print("List Transformations")
+#filter function
+over_five=list(filter(lambda x: x> 5, listA))
+print(f"Over five: {over_five}")
+
+#map function
+print("Map cuberoot")
+cuberoot_listA=list(map(lambda x: math.cbrt(x),listA))
+print(f"The cubed root is : {cuberoot_listA}")
+
+#map to caluclate volume
+print("Calculate volume")
+cube_vol=list(map(lambda x: (x*x*x), listA))
+
+#Task 6 Transformations
+print("List Transformations- List Comprehension")
+
+above_eight=[number for number in listA if number >8]
+print(f"This is the list with ages over 8: {above_eight}")
+
+#triple value
+listA_tripled=[round(number*3, 2) for number in listA]
+print(f"These are the tripled ages: {listA_tripled}")
+
+#my choice
+listA_add_six= [round(number+6,2) if number < 7 else number for number in listA]
+print(f"The new agesl list is: {listA_add_six}")
---
+++ user_numeric_lists_out.py
@@ -0,0 +1,47 @@
+List 1 Project 3
+mean:10.738095238095237
+mode:21
+median:10
+stdev:5.596023247849868
+variance:31.31547619047619
+List 2 Project 3
+List of ages for all orange cats in the shelter: [10, 9, 8, 7, 6, 4, 2, 1, 17, 18, 19]
+
+List of adoption prices of the orange cats in the shelter: [20.5, 19.5, 18, 25.5, 30.5, 22.5, 32.5, 31.5, 28.5, 45.5, 15.9]
+With linear regression if a cat is 25 years old, it will cost 26.865
+List number 3, Python Built in Functions
+min is:1
+max is:21
+len is:21
+sum is:225.5
+avg is:  10.738
+sorted: [1, 2, 4, 6, 6.5, 7, 7.5, 8, 8.5, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21]
+List 4, project 3,Methods
+New short list is: [10, 7, 3, 1]
+Append a single value to the list:
+[10, 7, 3, 1, 8]
+Extend with a new list:
+[10, 7, 3, 1, 8, 6, 5, 9]
+Lst with insert: [10, 7, 3, 1, 8, 6, 5, 9, 14]
+[10, 7, 3, 1, 8, 6, 9, 14]
+count how many times there is a 2 in the list
+0
+None
+This is the list sorted in descending order)
+None
+This is a copy of the list
+[14, 10, 9, 8, 7, 6, 3, 1]
+List with the first item removed
+[10, 9, 8, 7, 6, 3, 1]
+List with the last item removed
+[10, 9, 8, 7, 6, 3]
+List Transformations
+Over five: [21, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8.5, 8, 7.5, 7, 6.5, 6]
+Map cuberoot
+The cubed root is : [2.7589241763811208, 2.668401648721945, 2.6207413942088964, 2.571281590658235, 2.5198420997897464, 2.4662120743304703, 2.41014226417523, 2.3513346877207577, 2.2894284851066637, 2.2239800905693157, 2.154434690031884, 2.080083823051904, 2.040827550958674, 2.0, 1.9574338205844317, 1.9129311827723892, 1.866255578408624, 1.8171205928321397, 1.5874010519681996, 1.2599210498948732, 1.0]
+Calculate volume
+List Transformations- List Comprehension
+This is the list with ages over 8: [21, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8.5]
+These are the tripled ages: [63, 57, 54, 51, 48, 45, 42, 39, 36, 33, 30, 27, 25.5, 24, 22.5, 21, 19.5, 18, 12, 6, 3]
+The new agesl list is: [21, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8.5, 8, 7.5, 7, 12.5, 12, 10, 8, 7]
+(base) iMac:datafun-03-datatypes_project3 nistine$ 
